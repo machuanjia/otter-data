5b55c9a4a8e0a89783caeb43539888b77f6633f3
DayYoung <machuanjia@laiye.com>
2021-10-27 10:42:52 +0800

    chore: remove nav

--- src/layouts/Main/index.tsx
+++ src/layouts/Main/index.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: D.Y.M
  * @Date: 2021-10-20 16:35:49
- * @LastEditTime: 2021-10-25 17:07:08
- * @FilePath: /otter/src/layouts/Main/index.tsx
+ * @LastEditTime: 2021-10-27 10:42:05
+ * @FilePath: /otter-data/src/layouts/Main/index.tsx
  * @Description:
  */
 import React, { useEffect } from 'react'
@@ -13,13 +13,13 @@ import { useAppDispatch, useAppSelector } from '@/stores'
 import { selectStatus } from '@/stores/app'
 import AppService from '@/stores/app/app.service'
 
-import Help from '../Help'
-import Logo from '../Logo'
-import Nav from '../Nav'
-import Notice from '../Notice'
-import Preference from '../Preference'
-import Project from '../Project'
-import styles from './index.module.less'
+// import Help from '../Help'
+// import Logo from '../Logo'
+// import Nav from '../Nav'
+// import Notice from '../Notice'
+// import Preference from '../Preference'
+// import Project from '../Project'
+// import styles from './index.module.less'
 
 const Main = () => {
   const status = useAppSelector(selectStatus)
@@ -32,7 +32,7 @@ const Main = () => {
   }
   return (
     <section className="h-screen flex flex-row">
-      <nav className={`${styles['main-nav']} flex flex-col items-center`}>
+      {/* <nav className={`${styles['main-nav']} flex flex-col items-center`}>
         <header className=" pt-2 flex flex-col items-center">
           <Logo />
           <Project />
@@ -45,7 +45,7 @@ const Main = () => {
           <Help />
           <Preference />
         </footer>
-      </nav>
+      </nav> */}
       <div className="flex-1">
         <AsyncRoutes />
       </div>
