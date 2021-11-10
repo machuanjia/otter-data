a90bc28b5b6e361324e5c6427f6c234e9eb6f7ad
DayYoung <machuanjia@laiye.com>
2021-11-10 19:49:44 +0800

    feat: change

--- src/stores/app/app.service.ts
+++ src/stores/app/app.service.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: D.Y.M
  * @Date: 2021-10-25 14:38:42
- * @LastEditTime: 2021-11-08 11:06:49
+ * @LastEditTime: 2021-11-10 19:30:30
  * @FilePath: /otter-data/src/stores/app/app.service.ts
  * @Description:
  */
@@ -12,8 +12,11 @@ import { getUserInfo } from '@/apis'
 import { getPermissionsRouters } from '@/routes'
 import asyncRoutes from '@/routes/async'
 
+// import { store } from '../store'
+
 const AppService = {
   getInfo: createAsyncThunk('app/fetchInfo', async () => {
+    // console.log(store.getState().app.status)
     const data = await getUserInfo()
     // @ts-ignore
     const routes = getPermissionsRouters(asyncRoutes, data.permissions)
